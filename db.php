e50f956d2e1664628d691993ef30cbba950163b2
Naufal Adam Noorrahmat <n.adam28895@gmail.com>
2025-06-16 14:17:36 +0700

    db.php

--- db.php
+++ db.php
@@ -1,9 +1,10 @@
 <?php
 // Konfigurasi database
-$host = 'localhost';
+$host = 'db.be-mons1.bengt.wasmernet.com';
+$Port: '3306';
 $dbname = 'db_login';    // ganti sesuai database kamu
-$username = 'root';         // default user XAMPP
-$password = '';             // kosongkan jika tidak ada password
+$username = 'fc49728c7e60800026636bc170db';         // default user XAMPP
+$password = '0684fc49-728d-7028-8000-5a0b2d3fae8e';             // kosongkan jika tidak ada password
 
 // Data Source Name (DSN)
 $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8mb4";
@@ -18,13 +19,13 @@ $options = [
 // Buat koneksi PDO
 try {
 
-    
+
     $pdo = new PDO($dsn, $username, $password, $options);
 } catch (PDOException $e) {
     echo json_encode([
         "success" => false,
         "error" => "Database connection failed: " . $e->getMessage()
-        
+
     ]);
     exit;
 }
